cf6ca6dd032185cd05ba7d7b6caaa677c20c0106
Masskarad <masskaarad@gmail.com>
2016-11-28 16:29:02 +0100

    commit

---
+++ src/Formule.java
@@ -0,0 +1,14 @@
+
+public class Formule
+{
+	private String formule_de_base;
+	
+	public Formule(String p_formule)
+	{
+		formule_de_base = p_formule;
+	}
+	
+	public void Decomposition(){
+		
+	}
+}

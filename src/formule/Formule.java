ef2ece5df3c648f1b9b842e761eb681ec2b91882
Masskarad <masskaarad@gmail.com>
2016-12-05 16:56:44 +0100

    commit

--- src/formule/Formule.java
+++ src/formule/Formule.java
@@ -1,14 +1,17 @@
+package formule;
 
 public class Formule
 {
 	private String formule_de_base;
 	
+	
 	public Formule(String p_formule)
 	{
 		formule_de_base = p_formule;
 	}
-	
-	public void Decomposition(){
+
+	public void Decomposition()
+	{
 		
 	}
 }
